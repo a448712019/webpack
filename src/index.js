b146723a4f6de625125bf80e296de947196de0ef
catalina <sunjinpeng@puying.com>
2021-06-08 09:42:09 +0800

    更新

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 import './index.less'
-ReactDOM.render(<div className='box'>hello react webpack</div>, document.getElementById('root'))
+ReactDOM.render(<div className='box'>hello react webpack23</div>, document.getElementById('root'))
